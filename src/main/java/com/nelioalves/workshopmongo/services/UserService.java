a901f06aef68b3245fad369d667fd07057f1e68e
Arivaldo do Nascimento Silva <arivaldon.silva@gmail.com>
2018-08-30 15:19:41 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -29,7 +29,14 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);// para verificar se existe o id gerando excecao
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
+
+	
 }
